ecfbfd7841440166d039f992e0db1a44473474fb
Fabrice Million <fabrice@funkadelik.com>
2018-07-30 15:19:45 +1000

    temp fix to dynamycally import images, TODO: move that in the store

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,11 +1,21 @@
+/* eslint-disable */
 import React from 'react';
 import style from './card.scss';
 
 const Card = (props) => {
-  const { title } = props;
+  const { data } = props;
+  const importAll = (r) => {
+    const images = {};
+    r.keys().map(item => images[item.replace('./', '')] = r(item));
+    return images;
+  }
+  
+  const images = importAll(require.context('../../../assets/img', false, /\.(png|jpe?g|svg)$/));
+
+  console.log('img: ', images);
   return (
     <div className={style.card}>
-        {title}
+        <img src={images[data.productImage]} alt="" />
     </div>
   );
 };
